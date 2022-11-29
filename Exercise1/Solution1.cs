c5bb6b615a365389800bd47a9d5ef313ac4b6c8b
RuizJpaul <117695167+RuizJpaul@users.noreply.github.com>
2022-11-28 21:39:48 -0500

    Add files via upload

---
+++ Exercise1/Solution1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Solution1
+    {
+        static void Main(string[] args)
+        {
+            Random var = new Random();
+            int q, j = 0;
+            Console.Write("Enter the number of random numbers to evaluate: ");
+            q = int.Parse(Console.ReadLine());
+            for (int i = 0; i < q; i++)
+            {
+                //In this situation, we're only print palindrome numbers of 3 characters 
+                int num = var.Next(100,1000);
+                //we convert int to string so we can use "Substring" to catch the principal
+                //and last character
+                string chain = Convert.ToString(num);
+                string ans1 = chain.Substring(0,1);
+                string ans2 = chain.Substring(2,1);
+                if (ans1 == ans2)
+                {
+                    //we highlight the number to tell apart from the others
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(num);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    j++;
+                }
+                else
+                { Console.WriteLine(num); }
+            }
+            Console.WriteLine("\n# capicua numbers --> " + j);
+        }
+    }
+}
---
+++ Exercise1/Solution2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Solution2
+    {
+        static void Main(string[] args)
+        {
+            Random var = new Random();int j = 0;
+            Console.Write("Enter the number of random numbers to evaluate: ");
+            int q = int.Parse(Console.ReadLine());
+            for (int i = 0; i < q; i++)
+            {
+                //In this situation, we're only print palindrome numbers of 3 characters 
+                int ans = var.Next(100, 1000);
+                //we use the form of the division equation to make a new one linked to our purpose 
+                if (ans%10==(ans-ans%100)/100)
+                {
+                    //we highlight the number to tell apart from the others 
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ans);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    j++;
+                }
+                else
+                { Console.WriteLine(ans); }
+            }
+            Console.WriteLine("\n# capicua numbers: " + j);
+        }
+    }
+}
